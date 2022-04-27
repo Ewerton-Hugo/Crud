bc7983a40ca2f1172196097b39bdb23a469d2568
Ewerton_Hugo <ewerton.hugo@souunit.com.br>
2022-04-26 21:10:36 -0300

    CRUD completo

---
+++ main2.js
@@ -0,0 +1,153 @@
+//const client = require("pg/lib/native/client")
+
+//const { Client } = require("pg/lib")
+
+//'use strict'
+const openModal = () => document.getElementById('modal')
+    .classList.add('active')
+
+
+
+const closeModal = () => {
+    clearFields()
+    document.getElementById('modal').classList.remove('active')
+}
+const tempClient ={
+    nome: "Hugo",
+    email: "eamail",
+    celular: "",
+    cidade: "",
+}
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
+const setLocalStorage = (dbClient) => localStorage.setItem('db_client', JSON.stringify(dbClient))
+ 
+//CRUD  - create read update delet
+const deleteClient = (index) =>{
+    const dbClient = readClient()
+    dbClient.splice(index,1)
+    setLocalStorage(dbClient)
+}
+const updateCliente = (index, client) => {
+    const dbClient = readClient()
+    dbClient[index] = client
+    setLocalStorage(dbClient)
+
+}
+const readClient = () => getLocalStorage()
+
+const createClient = (client) => {
+    const dbClient = getLocalStorage()
+    dbClient.push (client)
+    setLocalStorage(dbClient)
+}
+//Interação com o leyout
+isValidFields = () =>{
+    return document.getElementById('form').reportValidity()
+}
+const clearFields = () =>{ 
+    const fields = document.querySelectorAll('.modal-field')
+    fields.forEach(field => field.value = "")
+}
+
+const saveClient = () =>{
+    if(isValidFields()){
+        const client = {
+            nome: document.getElementById('nome').value,
+            email: document.getElementById('email').value,
+            celular:document.getElementById('celular').value,
+            cidade:document.getElementById('cidade').value,
+        }
+        const index = document.getElementById('nome').dataset.index
+
+        if(index == 'new'){
+            createClient(client)
+            clearFields()
+            updateTable()
+            closeModal()
+        }
+        else{
+            updateCliente(index, client)
+            updateTable()
+            closeModal()
+        }
+
+    }
+}
+const createRow = (client,index) =>{
+    const newRow = document.createElement('tr')
+    newRow.innerHTML = `
+        <td>${client.nome}</td>
+        <td>${client.email}</td>
+        <td>${client.celular}</td>
+        <td>${client.cidade}</td>
+        <td>
+            <button type = "button" class="button green" id ="edit-${index}">Editar</button>
+            <button type = "button" class="button red" id ="delete-${index}">Excluir</button>
+        </td>
+
+        `
+    document.querySelector("#tableClient>tbody").appendChild(newRow)
+}
+const clearTable = () =>{
+    const rows = document.querySelectorAll('#tableClient>tbody tr')
+    rows.forEach(row => row.parentNode .removeChild(row))
+}
+const updateTable = () =>{
+    const dbClient = readClient()
+    clearTable()
+    dbClient.forEach(createRow)
+}
+fillFields = (client) =>{
+    document.getElementById('nome').value = client.nome
+    document.getElementById('email').value = client.email
+    document.getElementById('celular').value = client.celular
+    document.getElementById('cidade').value = client.cidade
+    document.getElementById('nome').dataset.index = client.index
+
+}
+const editClient = (index) =>{
+    const client = readClient()[index]
+    client.index = index
+    fillFields(client)
+    openModal()
+}
+const editDelete = (event) =>{
+    if(event.target.type == 'button'){
+        //alert(event.target.id) 
+        const [action, index] = event.target.id.split('-')
+        if(action == 'edit'){
+            editClient(index)
+
+        }
+        else{
+            const client = readClient()[index]
+            const response = confirm(`Deseja realmente excluir o cliente ${client.nome}`)
+            if(response){
+            deleteClient(index)
+            updateTable()
+            }
+            else{
+
+            }
+        }
+    }
+}
+updateTable()
+// Eventos
+document.getElementById('cadastrarCliente')
+    .addEventListener('click', openModal)
+
+document.getElementById('modalClose')
+    .addEventListener('click', closeModal)
+
+document.getElementById('salvar')
+    .addEventListener('click', saveClient)
+document.querySelector('#tableClient>tbody')
+    .addEventListener('click', editDelete)
+/*
+document.querySelector('#tableClient>tbody')
+    .addEventListener('click', editDelete)
+
+document.getElementById('cancelar')
+    .addEventListener('click', closeModal)*/
